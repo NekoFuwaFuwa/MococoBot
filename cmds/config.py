5fe991874ee9620a9a3bcf0984719c95897f6ee3
Okayu <166947665+NekoFuwaFuwa@users.noreply.github.com>
2024-05-19 03:37:12 -0500

    Update config.py
    Added more id entry's

--- cmds/config.py
+++ cmds/config.py
@@ -1,7 +1,12 @@
 
-
+# channel id's
+guild_id = 123
 welcome_channel_id = 123
 memes_channel_id = 123
+logs_channel_id = 123
+verify_channel_id = 123
 
+#role id's
+verify_role_id = 123
 def token():
     return "123"
